97cd0e04d7890aac4c0aed9d5912e32c82b31df1
opilane <opilane@HP-CZC0359NP1>
2024-05-16 10:07:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace switchkadak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int num = 0;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("arvo kadak vaga zesty ja gey");
+                    break;
+                case 2:
+                    Console.WriteLine("arvo kadak soob sulti enda vanaema keldris");
+                    break;
+                case 3:
+                    Console.WriteLine("arvo kadak likes grandparent succ");
+                    break;
+                default:
+                    Console.WriteLine("arvo kadak is not valid");
+                    break;
+            }
+        }
+
+    }
+}
